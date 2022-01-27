c272495a4228e0f73069d6582db8dbb2f5498790
MSKY <sharaiz.khan@empglabs.com>
2022-01-27 17:08:03 +0500

    Random_Number_Generator.js Update

--- cypress/integration/Random Number Generator/Random_Number_Generator.js
+++ cypress/integration/Random Number Generator/Random_Number_Generator.js
@@ -1,8 +1,4 @@
-
-// class Random_Number_Generator {
-
+// Random Number Generator
 export function getRndNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// }
